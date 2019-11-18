273696a58c856264f898f07ce89d5455400f1fcc
CodyThomasCrump <codytcrump@gmail.com>
2019-11-18 12:25:31 -0600

    Added the functionality to pull a random joke from our personal jokes and removed the placeholder joke.

--- src/PersonalJokes/OurPunchLines.js
+++ src/PersonalJokes/OurPunchLines.js
@@ -102,7 +102,173 @@ const punchLines = [
         "setup": "Why do the French eat snails?",
         "punchline": "They don't like fast food"
     }, {
-        
+        "setup": "What's the difference between a poorly dressed man on a bicycle and a nicely dressed man on a tricycle?",
+        "punchline": "A tire."
+    }, {
+        "setup": "Can a kangaroo jump higher than the Empire State Building?",
+        "punchline": "Of course. The Empire State Building can't jump."
+    }, {
+        "setup": "I ordered 2000 lbs. of chinese soup.",
+        "punchline": "It was Won Ton"
+    }, {
+        "setup": "What do you call a dictionary on drugs?",
+        "punchline": "HIGH-Definition."
+    }, {
+        "setup": "A courtroom artist was arrested today for an unknown reason",
+        "punchline": "Details are sketchy."
+    }, {
+        "setup": "I made a mistake at the grocery store.",
+        "punchline": "I went to get 6 Sprites. Accidentally picked 7up"
+    }, {
+        "setup": "My boss told me to have a good day",
+        "punchline": "So I went home."
+    }, {
+        "setup": "Did you hear about these new reversible jackets?",
+        "punchline": "I'm excited to see how they turn out."
+    }, {
+        "setup": "My girlfriend said, You act like a detective too much. I want to split up.",
+        "punchline": "Good idea, I replied. We can cover more ground that way."
+    }, {
+        "setup": "I finally realized my parents favored my twin brother.",
+        "punchline": "It hit me when they asked me to blow up balloons for his surprise birthday party."
+    }, {
+        "setup": "I've just written a song about tortillas",
+        "punchline": "Actually, it's more of a wrap."
+    }, {
+        "setup": "Got my girlfriend a get better soon card.",
+        "punchline": "She's not sick, I just think she could get better."
+    }, {
+        "setup": "I bought a dictionary and when I got home I realized all the pages were blank",
+        "punchline": "I have no words for how angry I am."
+    }, {
+        "setup": "I'm reading a horror story in Braille.",
+        "punchline": "Something bad is about to happen... I can feel it."
+    }, {
+        "setup": "Someone stole my toilet",
+        "punchline": "The police have nothing to go on."
+    }, {
+        "setup": "I was born to be a pessimist.",
+        "punchline": "My blood type is B Negative."
+    }, {
+        "setup": "Did you hear about that guy who fell into the infinity pool?",
+        "punchline": "It took him forever to get out."
+    }, {
+        "setup": "What do you call a cow with no legs?",
+        "punchline": "Ground beef."
+    }, {
+        "setup": "I found a rock yesterday which measured 1760 yards in length.",
+        "punchline": "Must be some kind of milestone."
+    }, {
+        "setup": "When I was young, I always felt like a male trapped in a females body.",
+        "punchline": "Then I was born"
+    }, {
+        "setup": "The therapist asked my wife why she wanted to end our marriage. She said she hated all the constant Star Wars puns.",
+        "punchline": "I look at the therapist and said, Divorce is strong with this one!"
+    }, {
+        "setup": "What do you call people who are afraid of Santa Claus?",
+        "punchline": "Claustrophobic"
+    }, {
+        "setup": "What do you call a laughing motorcycle?",
+        "punchline": "A Yamahahaha."
+    }, {
+        "setup": "What do you call Chewbacca when he has chocolate stuck in his hair?",
+        "punchline": "Chocolate Chip Wookiee."
+    }, {
+        "setup": "What happens if you eat yeast and shoe polish?",
+        "punchline": "Every morning you will rise and shine!"
+    }, {
+        "setup": "How do you unlock doors on Kashyyyk?",
+        "punchline": "With a wookiee."
+    }, {
+        "setup": "My math teacher called me average.",
+        "punchline": "How mean!"
+    }, {
+        "setup": "I own a pencil that used to be owned by William Shakespeare, but he chewed it a lot.",
+        "punchline": "Now I can't tell if it's 2B or not 2B."
+    }, {
+        "setup": "A book just fell on my head.",
+        "punchline": "I've only got myshelf to blame."
+    }, {
+        "setup": "What did one eye say to the other eye?",
+        "punchline": "Between you and me something smells."
+    }, {
+        "setup": "I am on a seafood diet.",
+        "punchline": "Every time I see food, I eat it."
+    }, {
+        "setup": "Why couldn't the bike stand up on it's own?",
+        "punchline": "It was two tired."
+    }, {
+        "setup": "This morning some clown opened the door for me.",
+        "punchline": "I thought to myself that's a nice Jester."
+    }, {
+        "setup": "Did you hear about the Italian chef with a terminal illness?",
+        "punchline": "He pastaway."
+    }, {
+        "setup": "Why did the scientist install a knocker on his door?",
+        "punchline": "He wanted to win the No-bell prize!"
+    }, {
+        "setup": "What do prisoners use to call each other?",
+        "punchline": "Cell phones."
+    }, {
+        "setup": "For Halloween we dressed up as almonds.",
+        "punchline": "Everyone could tell we were nuts."
+    }, {
+        "setup": "Recently, I've tried to make a car without wheels.",
+        "punchline": "I've been working on it tirelessly."
+    }, {
+        "setup": "Did you hear about the man who jumped off a bridge in France?",
+        "punchline": "He was in Sein."
+    }, {
+        "setup": "What do you call a priest that becomes a lawyer?",
+        "punchline": "A father in law."
+    }, {
+        "setup": "Why don't aliens visit our planet?",
+        "punchline": "Terrible ratings. One star."
+    }, {
+        "setup": "As a wizard, I enjoy turning objects into a glass.",
+        "punchline": "Just wanted to make that clear."
+    }, {
+        "setup": "I dated a girl with a lazy eye once.",
+        "punchline": "It turns out she was seeing someone else on the side."
+    }, {
+        "setup": "What do you call a boat full of polite football players?",
+        "punchline": "A good sportsman ship."
+    }, {
+        "setup": "What was Forrest Gump's email password?",
+        "punchline": "1forrest1"
+    }, {
+        "setup": "How did Jesus' crucifixion save us?",
+        "punchline": "It's because he nailed it!"
+    }, {
+        "setup": "What do you have to do to have a party in space?",
+        "punchline": "You have to Planet."
+    }, {
+        "setup": "People don't get my puns.",
+        "punchline": "They think they're funny."
+    }, {
+        "setup": "Why couldn't the leopard play hide and seek?",
+        "punchline": "Because he was always spotted."
+    }, {
+        "setup": "How do trees access the internet?",
+        "punchline": "They log in."
+    }, {
+        "setup": "Why do seagulls fly over the sea?",
+        "punchline": "Because if they flew over the bay they'd be bagels!"
+    }, {
+        "setup": "Why did the bee get married?",
+        "punchline": "Because he found his honey."
+    }, {
+        "setup": "Heard about the drug addict fisherman who accidentally caught a duck?",
+        "punchline": "Now he's hooked on the quack."
+    }, {
+        "setup": "Do German cats have multiple lives?",
+        "punchline": "Nein"
+    }, {
+        "setup": "Police have arrested the World tongue-twister Champion.",
+        "punchline": "I imagine he'll be given a tough sentence."
+    }, {
+        "setup": "Three conspiracy theorists walk into a bar.",
+        "punchline": "You can't tell me that's just a coincidence!"
     }]
 
 export default punchLines;
